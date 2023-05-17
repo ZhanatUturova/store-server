2014080109e1b96986152571fa704470174448db
Zhanat Uturova <526628@gmail.com>
2023-05-18 00:00:54 +0600

    Add total sum and quantity in basket

--- users/views.py
+++ users/views.py
@@ -44,10 +44,20 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
+    
+    # baskets = Basket.objects.filter(user=request.user)
+    # total_sum = 0
+    # total_quantity = 0
+    # for basket in baskets:
+    #     total_sum += basket.sum()
+    #     total_quantity += basket.quantity
+
     context = {
         'title': 'Store - Профиль', 
         'form': form,
-        'baskets': Basket.objects.filter(user=request.user)
+        'baskets': Basket.objects.filter(user=request.user),
+        # 'total_sum': sum(basket.sum() for basket in baskets),
+        # 'total_quantity': sum(basket.quantity for basket in baskets),
         }
     return render(request, 'users/profile.html', context)
 
